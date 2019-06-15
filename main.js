58e92469d93bb278a5a01afe27290bef7881ae66
Denis Kisina <kisinad@gmail.com>
2019-06-15 18:17:46 +0300

    removed commented code lines and number to guess

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var i = Math.floor(Math.random() * 100) + 1; //Generates random numbers and stor
 
 var j=i%2; //gets the modulus and stores the result in j
 if (j==0) //Conitioin checks to see if the result in module is zero or not
-	alert (i +" Number is Even"); 
+	alert (" Number is Even"); 
 else 
-	alert (i +" Number is odd");
+	alert (" Number is odd");
 
 for(var count=1;count<=10;count++)
 {
@@ -23,44 +23,6 @@ for(var count=1;count<=10;count++)
 	if (count==10){	
 			alert("You LOSE!");}
 }
-
-/*if (num==i)
-	//break;
-	alert("You WIN!");
-	
-else 
-	{
-		if (num<i)
-			alert("Number is lower, make another guess");
-		else if (num>i)
-			alert("Number is higher,make another guess");
-		else	
-			alert("You LOSE!");
-	}*/
-
-
-//else	
-//alert("You LOSE!");
-
-//alert("You LOSE!");
-	
-
-	
-/*	var count=1;
-while (count<=10){
-	let num = prompt ("Please enter a number:"); //Outputs prompt window
-	
-	if (num>i)
-	alert("Number is higher,make another guess");
-//else if (num<i)
-//	alert("Number is lower, make another guess");
-//else if (num==i)
-//	alert("You WIN!");
-else
-	alert("You LOSE!");
-
-}*/
-	
 
 
 
